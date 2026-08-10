1d520e3a202edd0430f95e78051725cf6ae98010
mirroringcode <miguelolivar999@gmail.com>
2026-08-10 11:14:12 -0400

    updated gitignore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,45 +1,16 @@
 fun main() {
 
 
-    println("How many numbers will you enter")
-    val amountOfNumbers = readln().toIntOrNull() ?: 0;
-//    var i = 0
-
-    val numbers = mutableListOf<Int>()
-    var sum = 0
-
-    for (i in 0 until amountOfNumbers) {
-        println("Please enter number #${i + 1}")
-        val number = readln().toIntOrNull() ?: continue
-        numbers.add(number)
-        sum += number
+    println("Enter a string:")
+    val input = readln()
+    val finalString = buildString {
+        for (i in input.lastIndex downTo 0) {
+            append(input[i])
+        }
     }
 
+    println(finalString)
 
-    for (number in numbers) {
-        println(number)
-    }
-
-
-
-
-
-
-
-/*    var sum = 0
-    var i = 0;
-    var numbers = intArrayOf();*/
-/*    while (i < amountOfNumbers) {
-        println("Please enter number #${i + 1}")
-        val number = readln().toIntOrNull() ?: 0;
-
-        sum += number
-        numbers += number;
-        i++
-    }*/
-
-    println("Numbers : ${numbers}")
-    println("The total sum is $sum")
 
 
 }

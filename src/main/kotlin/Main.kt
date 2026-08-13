c9ac28b532c304d5773117ce1289e44d01539850
mirroringcode <miguelolivar999@gmail.com>
2026-08-13 09:32:40 -0400

    Lambda functions 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,15 +6,23 @@ fun main() {
     val favoriteNumbers = intArrayOf(21, 34, 56, 78)
     val evenNumbers = favoriteNumbers.filter { it % 2 == 0}
 
-/*    val lettersonly = input.filter {
-        it.isLetter()
-    }*/
+
 
     val lambda: (Char) -> Boolean = {
         it.isLetter()
     }
 
+    val lettersOnly = input.filter {
+        it.isLetter()
+    }
+
+//    val lettersOnly = input.filter(lambda)
+
+/*    val lettersOnly = input.filter { currentCharacter ->
+        currentCharacter.isLetter();
+    }*/
+
     println(evenNumbers)
-    print(lettersonly)
+    print(lettersOnly)
 
 }
